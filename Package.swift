399ebac451c9d04601774f2d7fb5334a591a1c01
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-07 17:30:59 +0800

    ⚙️ Chore: Update SPM Config

--- Package.swift
+++ Package.swift
@@ -1,22 +1,38 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// The swift-tools-version declares the minimum version of Swift required to 
+// build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "ContextMenuAuxiliaryPreview",
+  platforms: [
+    .iOS(.v13),
+  ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
+    // Products define the executables and libraries a package produces, making 
+    // them visible to other packages.
     .library(
       name: "ContextMenuAuxiliaryPreview",
       targets: ["ContextMenuAuxiliaryPreview"]
     ),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
+    // Targets are the basic building blocks of a package, defining a module or 
+    // a test suite.
+    //
+    // Targets can depend on other targets in this package and products from 
+    // dependencies.
     .target(
-      name: "ContextMenuAuxiliaryPreview"
+      name: "ContextMenuAuxiliaryPreview",
+      path: "Sources",
+      dependencies: [],
+      linkerSettings: [
+				.linkedFramework("UIKit"),
+			]
     ),
+  ],
+  swiftLanguageVersions: [
+    .v5,
   ]
 );
