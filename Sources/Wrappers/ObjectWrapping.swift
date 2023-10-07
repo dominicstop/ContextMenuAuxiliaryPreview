e64c7c9f33fc2b7302a97bb43bcec97316e5efb5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-08 00:08:18 +0800

    🆕 Add: Import Sources - Wrappers

---
+++ Sources/Wrappers/ObjectWrapping.swift
@@ -0,0 +1,26 @@
+//
+//  ObjectWrapping.swift
+//  
+//
+//  Created by Dominic Go on 9/18/23.
+//
+
+import Foundation
+
+protocol ObjectWrapping {
+  associatedtype WrapperType;
+  
+  var objectWrapper: ObjectWrapper<WrapperType> { get set };
+};
+
+extension ObjectWrapping {
+  var wrappedObject: WrapperType? {
+    self.objectWrapper.object;
+  };
+  
+  func debugPrintWrappedObject(){
+    #if DEBUG
+    print(self.wrappedObject.debugDescription);
+    #endif
+  };
+};
