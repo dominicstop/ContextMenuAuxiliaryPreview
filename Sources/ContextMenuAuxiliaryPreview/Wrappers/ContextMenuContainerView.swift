4a08eba63df8f6f578c01f7cc7bfad52ff355196
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-09 01:47:48 +0800

    ⭐️ Impl: `ContextMenuContainerView `

---
+++ Sources/ContextMenuAuxiliaryPreview/Wrappers/ContextMenuContainerView.swift
@@ -0,0 +1,39 @@
+//
+//  ContextMenuContainerView.swift.swift
+//  
+//
+//  Created by Dominic Go on 10/8/23.
+//
+
+import UIKit
+
+
+/// Wrapper for: _UIContextMenuContainerView
+class ContextMenuContainerViewWrapper:
+  ObjectWrapperBase<UIView, ContextMenuContainerViewWrapper.EncodedString> {
+
+  enum EncodedString: String, ObjectWrappingEncodedString {
+    case className;
+    
+    var encodedString: String {
+      switch self {
+        case .className:
+          // _UIContextMenuContainerView
+          return "X1VJQ29udGV4dE1lbnVDb250YWluZXJWaWV3";
+      };
+    };
+};
+
+  
+  // MARK: - Computed Properties
+  // ---------------------------
+  
+  var backgroundVisualEffectView: UIVisualEffectView? {
+    guard let view = self.wrappedObject else { return nil };
+    
+    return view.subviews.reduce(nil){
+      $0 ?? ($1 as? UIVisualEffectView)
+    };
+  };
+  
+};
