ba63fb7f0df6e219aabcdf4c04f04a14f63da020
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-21 01:22:43 +0800

    💫 Update: `ContextMenuAuxiliaryPreviewConfig`

--- Sources/ContextMenuAuxiliaryPreview/ContextMenuManager/ContextMenuAuxiliaryPreviewConfig.swift
+++ Sources/ContextMenuAuxiliaryPreview/ContextMenuManager/ContextMenuAuxiliaryPreviewConfig.swift
@@ -8,21 +8,21 @@ import Foundation
 import UIKit
 
 
-struct ContextMenuAuxiliaryPreviewConfig {
+public struct ContextMenuAuxiliaryPreviewConfig {
   
   // MARK: - Nested Types
   // --------------------
   
-  enum AnchorPosition: String {
+  public enum AnchorPosition: String {
     case top, bottom, automatic;
   };
   
-  enum HorizontalAlignment: String {
+  public enum HorizontalAlignment: String {
     case stretchScreen, stretchPreview;
     case previewLeading, previewTrailing, previewCenter;
   };
   
-  enum TransitionType {
+  public enum TransitionType {
     case none, fade;
     
     case slide(slideOffset: CGFloat);
@@ -30,37 +30,28 @@ struct ContextMenuAuxiliaryPreviewConfig {
     case zoomAndSlide(slideOffset: CGFloat, zoomOffset: CGFloat);
   };
   
-  struct TransitionConfig {
-    static let `default`: Self = .init(
+  public struct TransitionConfig {
+    public static let `default`: Self = .init(
       transition: .fade,
       duration: 0.3,
       delay: 0,
       options: []
     );
   
-    var transition: TransitionType;
+   public var transition: TransitionType;
     
-    var duration: CGFloat;
-    var delay: CGFloat;
-    var options: UIView.AnimationOptions;
+   public var duration: CGFloat;
+   public var delay: CGFloat;
+   public var options: UIView.AnimationOptions;
   };
   
-  enum TransitionEntranceDelay: Equatable {
+  public enum TransitionEntranceDelay: Equatable {
     case seconds(CGFloat);
     
     case RECOMMENDED;
     case AFTER_PREVIEW;
     
-    init?(string: String){
-      switch string {
-        case "RECOMMENDED"  : self = .RECOMMENDED;
-        case "AFTER_PREVIEW": self = .AFTER_PREVIEW;
-          
-        default: return nil;
-      };
-    };
-    
-    var seconds: CGFloat {
+    public var seconds: CGFloat {
       switch self {
         case .seconds(let seconds):
           return seconds;
@@ -74,18 +65,39 @@ struct ContextMenuAuxiliaryPreviewConfig {
   // MARK: - Properties
   // ------------------
   
-  var height: CGFloat?;
-  var width: CGFloat?;
+  public var height: CGFloat?;
+  public var width: CGFloat?;
 
-  var anchorPosition: AnchorPosition;
-  var alignmentHorizontal: HorizontalAlignment;
+  public var anchorPosition: AnchorPosition;
+  public var alignmentHorizontal: HorizontalAlignment;
   
-  var marginPreview: CGFloat;
-  var marginAuxiliaryPreview: CGFloat;
+  public var marginPreview: CGFloat;
+  public var marginAuxiliaryPreview: CGFloat;
 
-  var transitionConfigEntrance: TransitionConfig;
-  var transitionEntranceDelay: TransitionEntranceDelay;
+  public var transitionConfigEntrance: TransitionConfig;
+  public var transitionEntranceDelay: TransitionEntranceDelay;
+  
+  // MARK: - Init
+  // ------------
+  
+  public init(
+    height: CGFloat? = nil,
+    width: CGFloat? = nil,
+    anchorPosition: AnchorPosition,
+    alignmentHorizontal: HorizontalAlignment,
+    marginPreview: CGFloat,
+    marginAuxiliaryPreview: CGFloat,
+    transitionConfigEntrance: TransitionConfig,
+    transitionEntranceDelay: TransitionEntranceDelay
+  ) {
+  
+    self.height = height
+    self.width = width
+    self.anchorPosition = anchorPosition
+    self.alignmentHorizontal = alignmentHorizontal
+    self.marginPreview = marginPreview
+    self.marginAuxiliaryPreview = marginAuxiliaryPreview
+    self.transitionConfigEntrance = transitionConfigEntrance
+    self.transitionEntranceDelay = transitionEntranceDelay
+  }
 };
-
-
-
