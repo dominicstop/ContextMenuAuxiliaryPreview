1a6159cf91822119b339971bb63a67abf326be57
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-21 01:22:20 +0800

    💫 Update: `ContextMenuMetadata`

--- Sources/ContextMenuAuxiliaryPreview/ContextMenuManager/ContextMenuMetadata.swift
+++ Sources/ContextMenuAuxiliaryPreview/ContextMenuManager/ContextMenuMetadata.swift
@@ -10,11 +10,12 @@ import UIKit
 
 public struct ContextMenuMetadata {
 
-  // amount to add to width - fix for layout bug
-  //
-  // if you use the actual width, it triggers a bug w/ autolayout where the
-  // aux. preview snaps to the top of the screen...
-  static let auxPreviewWidthAdj = 0.5;
+  /// amount to add to width - fix for layout bug
+  ///
+  /// if you use the actual width, it triggers a bug w/ autolayout where the
+  /// aux. preview snaps to the top of the screen...
+  ///
+  static let auxiliaryViewExtraWidth = 0.5;
 
   // MARK: - Embedded Types
   // ----------------------
@@ -23,139 +24,123 @@ public struct ContextMenuMetadata {
     case top;
     case bottom;
   };
-  
+
   // MARK: - Properties
   // ------------------
   
-  var auxPreviewConfig: ContextMenuAuxiliaryPreviewConfig;
+  let auxiliaryViewHeight: CGFloat;
+  let auxiliaryViewWidth: CGFloat;
   
-  /// Does the context menu have menu items?
-  public var hasMenuItems: Bool;
+  /// distance of aux. preview from the context menu preview
+  let marginInner: CGFloat;
   
-  /// If the context menu has "menu items", where is it located in relation to
-  /// the "menu preview"?
-  public var menuItemsPlacement: Position?;
+  /// distance of the aux. preview from the edges of the screen
+  let marginOuter: CGFloat;
+  
+  /// does the context menu have menu items?
+  let contextMenuHasMenuItems: Bool;
+  
+  /// if the context menu has "menu items", where is it located in relation to
+  ///  the "menu preview"?
+  let menuItemsPlacement: Position?;
   
   /// in which vertical half does the "context menu preview" fall into?
-  public var morphingPlatterViewPlacement: Position;
+  let morphingPlatterViewPlacement: Position;
   
-  /// Whether to attach the `auxiliaryView` on the top, or bottom of the
-  /// context menu
-  public var auxPreviewPlacement: Position;
+   /// whether to attach the `auxiliaryView` on the top or bottom of the
+   /// context menu
+  let shouldAttachAuxPreviewToTop: Bool;
   
-  /// The amount to nudge the context menu
-  public var auxPreviewOffsetY: CGFloat;
+  /// the amount to nudge the context menu
+  let contextMenuOffsetY: CGFloat;
   
-  public var auxPreviewHeight: CGFloat;
-  public var auxPreviewWidth: CGFloat;
+  // MARK: - Properties - References
+  // -------------------------------
+
+  var contextMenuContainerViewWrapper: ContextMenuContainerViewWrapper;
   
-  // MARK: - Computed Properties
-  // ---------------------------
+  var contextMenuPlatterTransitionViewWrapper:
+    ContextMenuPlatterTransitionViewWrapper;
+    
+  var morphingPlatterViewWrapper: MorphingPlatterViewWrapper;
   
-  public var auxPreviewSize: CGSize {
+  /// where should the aux. preview be attached to?
+  weak var auxPreviewTargetView: UIView?;
+  
+  // MARK: - Properties - Computed
+  // -----------------------------
+  
+  var auxiliaryPreviewViewSize: CGSize {
     .init(
-      width: self.auxPreviewWidth + Self.auxPreviewWidthAdj,
-      height: self.auxPreviewHeight
+      width : self.auxiliaryViewWidth + Self.auxiliaryViewExtraWidth,
+      height: self.auxiliaryViewHeight
     );
   };
   
   // MARK: - Init
   // ------------
   
-  init?(
-    window: UIWindow,
-    previewAuxiliaryView: UIView,
-    auxPreviewConfig: ContextMenuAuxiliaryPreviewConfig,
-    contextMenuContainerViewWrapper: ContextMenuContainerViewWrapper
-  ) {
+  public init?(manager: ContextMenuManager){
+    guard let menuAuxPreviewConfig = manager.menuAuxPreviewConfig,
+          let menuAuxiliaryPreviewView = manager.menuAuxiliaryPreviewView,
 
-    guard let contextMenuContainerViewWrapper =
+          let contextMenuContainerViewWrapper =
+            manager.contextMenuContainerViewWrapper,
+          
+          contextMenuContainerViewWrapper.wrappedObject != nil,
+        
+          let contextMenuPlatterTransitionViewWrapper =
             contextMenuContainerViewWrapper.contextMenuPlatterTransitionViewWrapper,
             
           let contextMenuContainerView =
             contextMenuContainerViewWrapper.wrappedObject,
-            
+      
           let morphingPlatterViewWrapper =
-            contextMenuContainerViewWrapper.morphingPlatterViewWrapper,
+            contextMenuPlatterTransitionViewWrapper.morphingPlatterViewWrapper,
             
           let morphingPlatterView =
             morphingPlatterViewWrapper.wrappedObject
+              
     else { return nil };
     
-    self.auxPreviewConfig = auxPreviewConfig;
-    
     let contextMenuViewWrapper =
-      contextMenuContainerViewWrapper.contextMenuViewWrapper;
-     
-    let hasMenuItems = contextMenuViewWrapper != nil;
-    self.hasMenuItems = hasMenuItems;
-     
-    let menuItemsPlacement: Position? = {
-      guard hasMenuItems,
-            let contextMenuView = contextMenuViewWrapper?.wrappedObject
-      else { return nil };
-      
-      let previewFrame = morphingPlatterView.frame;
-      let menuItemsFrame = contextMenuView.frame;
-      
-      return (menuItemsFrame.midY < previewFrame.midY)
-        ? .bottom
-        : .top;
-    }();
-    
-    self.menuItemsPlacement = menuItemsPlacement;
-    
-    let morphingPlatterViewPlacement: Position = {
-      let previewFrame = morphingPlatterView.frame;
-      let screenBounds = UIScreen.main.bounds;
-      
-      return (previewFrame.midY < screenBounds.midY)
-        ? .top
-        : .bottom;
-    }();
-    
-    self.morphingPlatterViewPlacement = morphingPlatterViewPlacement;
-    
-    self.auxPreviewPlacement = {
-      switch auxPreviewConfig.anchorPosition {
-        case .top   : return .top;
-        case .bottom: return .bottom;
+      contextMenuPlatterTransitionViewWrapper.contextMenuViewWrapper;
             
-        case .automatic:
-          break;
-      };
+    let contextMenuView = contextMenuViewWrapper?.wrappedObject;
+  
+    self.contextMenuContainerViewWrapper = contextMenuContainerViewWrapper;
+    
+    self.contextMenuPlatterTransitionViewWrapper =
+      contextMenuPlatterTransitionViewWrapper;
       
-      if let menuItemsPlacement = menuItemsPlacement {
-        return menuItemsPlacement;
-      };
+    self.morphingPlatterViewWrapper = morphingPlatterViewWrapper;
+  
+    let contextMenuHasMenuItems = contextMenuView != nil;
+    self.contextMenuHasMenuItems = contextMenuHasMenuItems;
+    
+    // MARK: Prep - Set Constants
+    // --------------------------
+
+    self.auxPreviewTargetView = manager.isUsingCustomMenuPreview
+      ? morphingPlatterView
+      : contextMenuContainerView;
+    
+    let auxiliaryViewHeight: CGFloat = {
+      // Begin inferring the height of the aux. view...
       
-      // the context menu does not have menu items, determine anchor position
-      // of auxiliary view via the position of the preview in the screen
-      return morphingPlatterViewPlacement;
-    }();
-    
-    /// distance of aux. preview from the context menu preview
-    let marginInner = auxPreviewConfig.marginPreview;
-    
-    /// distance of the aux. preview from the edges of the screen
-    let marginOuter = auxPreviewConfig.marginAuxiliaryPreview;
-    
-    let auxPreviewHeight: CGFloat = {
       // A - Use height from config
-      if let height = auxPreviewConfig.height {
+      if let height = menuAuxPreviewConfig.height {
         return height;
       };
       
-      // B - Infer aux preview height from view
-      return previewAuxiliaryView.frame.height;
+      // B - Use height from aux view
+      return menuAuxiliaryPreviewView.frame.height;
     }();
     
-    self.auxPreviewHeight = auxPreviewHeight;
-    
-    let auxPreviewWidth: CGFloat = {
+    let auxiliaryViewWidth: CGFloat = {
       // Begin inferring the width of the aux. view...
       
-      switch auxPreviewConfig.alignmentHorizontal {
+      switch menuAuxPreviewConfig.alignmentHorizontal {
         // A - Set aux preview width to window width
         case .stretchScreen:
           return contextMenuContainerView.frame.width;
@@ -166,14 +151,71 @@ public struct ContextMenuMetadata {
         
         // C - Infer aux config or aux preview width from view...
         default:
-          return auxPreviewConfig.width ?? previewAuxiliaryView.frame.width;
+          return menuAuxPreviewConfig.width
+            ?? menuAuxiliaryPreviewView.frame.width;
       };
     }();
     
-    self.auxPreviewWidth = auxPreviewWidth;
+    self.auxiliaryViewHeight = auxiliaryViewHeight;
+    self.auxiliaryViewWidth = auxiliaryViewWidth;
     
-    self.auxPreviewOffsetY = {
-      let safeAreaInsets = window.safeAreaInsets;
+    /// distance of aux. preview from the context menu preview
+    let marginInner = menuAuxPreviewConfig.marginPreview;
+    self.marginInner = marginInner;
+    
+    /// distance of the aux. preview from the edges of the screen
+    let marginOuter = menuAuxPreviewConfig.marginAuxiliaryPreview;
+    self.marginOuter = marginOuter;
+    
+    // MARK: Prep - Determine Size and Position
+    // ----------------------------------------
+    
+    let menuItemsPlacement: Position? = {
+      guard contextMenuHasMenuItems,
+            let contextMenuView = contextMenuView
+      else { return nil };
+      
+      let previewFrame = morphingPlatterView.frame;
+      let menuItemsFrame = contextMenuView.frame;
+      
+      return (menuItemsFrame.midY < previewFrame.midY) ? .bottom : .top;
+    }();
+    
+    self.menuItemsPlacement = menuItemsPlacement;
+    
+    let morphingPlatterViewPlacement: Position = {
+      let previewFrame = morphingPlatterView.frame;
+      let screenBounds = UIScreen.main.bounds;
+
+      return (previewFrame.midY < screenBounds.midY) ? .top : .bottom;
+    }();
+    
+    self.morphingPlatterViewPlacement = morphingPlatterViewPlacement;
+    
+    self.shouldAttachAuxPreviewToTop = {
+      switch menuAuxPreviewConfig.anchorPosition {
+        case .top   : return true;
+        case .bottom: return false;
+          
+        case .automatic: break;
+      };
+      
+      switch menuItemsPlacement {
+        case .top   : return true;
+        case .bottom: return false;
+          
+        default:
+          // the context menu does not have menu items, determine anchor position
+          // of auxiliary view via the position of the preview in the screen
+          return morphingPlatterViewPlacement == .bottom;
+      };
+    }();
+    
+    // MARK: Prep - Compute Offsets
+    // ----------------------------
+    
+    self.contextMenuOffsetY = {
+      let safeAreaInsets = UIApplication.shared.windows.first?.safeAreaInsets;
       
       let previewFrame = morphingPlatterView.frame;
       let screenHeight = UIScreen.main.bounds.height;
@@ -182,28 +224,30 @@ public struct ContextMenuMetadata {
       
       switch morphingPlatterViewPlacement {
         case .top:
-          let margin = marginBase + safeAreaInsets.top;
+          let topInsets = safeAreaInsets?.top ?? 0;
+          let margin = marginBase + topInsets;
           
-          let minEdgeY = auxPreviewHeight + safeAreaInsets.top + margin;
-          let distanceToEdge = auxPreviewWidth - previewFrame.minY;
+          let minEdgeY = auxiliaryViewHeight + topInsets + margin;
+          let distanceToEdge = auxiliaryViewHeight - previewFrame.minY;
         
           return (previewFrame.minY <= minEdgeY)
             ? max((distanceToEdge + margin), 0)
             : 0;
           
         case .bottom:
-          let margin = marginBase + safeAreaInsets.bottom;
+          let bottomInsets = safeAreaInsets?.bottom ?? 0;
+          let margin = marginBase + bottomInsets;
           
-          let tolerance = auxPreviewHeight + margin;
+          let tolerance = auxiliaryViewHeight + margin;
           let maxEdgeY = screenHeight - tolerance;
           let previewFrameMaxY = previewFrame.maxY + marginInner;
           
           let distanceToEdge = screenHeight - previewFrame.maxY;
           
           return (previewFrameMaxY > maxEdgeY)
-            ? -(auxPreviewHeight - distanceToEdge + margin)
+            ? -(auxiliaryViewHeight - distanceToEdge + margin)
             : 0;
       };
-  }();
+    }();
   };
 };
