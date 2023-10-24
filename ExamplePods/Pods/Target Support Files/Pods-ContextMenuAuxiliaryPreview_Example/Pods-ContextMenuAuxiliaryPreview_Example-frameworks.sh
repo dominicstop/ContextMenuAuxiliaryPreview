963ad0f555dc8a3321751a48e9217e7e204720d6
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-24 22:08:40 +0800

    ❌ Remove: Ex Pods - Pods and Related Sources

--- ExamplePods/Pods/Target Support Files/Pods-ContextMenuAuxiliaryPreview_Example/Pods-ContextMenuAuxiliaryPreview_Example-frameworks.sh
+++ ExamplePods/Pods/Target Support Files/Pods-ContextMenuAuxiliaryPreview_Example/Pods-ContextMenuAuxiliaryPreview_Example-frameworks.sh
@@ -177,9 +177,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ContextMenuAuxiliaryPreview/ContextMenuAuxiliaryPreview.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DGSwiftUtilities/DGSwiftUtilities.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ContextMenuAuxiliaryPreview/ContextMenuAuxiliaryPreview.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DGSwiftUtilities/DGSwiftUtilities.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- ExamplePods/Pods/Target Support Files/Pods-ContextMenuAuxiliaryPreview_Tests/Pods-ContextMenuAuxiliaryPreview_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ContextMenuAuxiliaryPreview_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ContextMenuAuxiliaryPreview_Tests
-@end
