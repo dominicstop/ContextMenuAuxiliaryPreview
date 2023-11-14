8848315df145d21b837b4e5fed1e3e61574575ba
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-15 05:50:08 +0800

    💫 Update: Ex - `AuxiliaryPreviewPresets`
    Related:
    * `TODO:2023-10-22-09-09-34` - Impl: Examples - Add VC for testing the different possible combinations of aux. preview config.

--- Example/Screens/AuxiliaryPreviewPresets.swift
+++ Example/Screens/AuxiliaryPreviewPresets.swift
@@ -26,12 +26,7 @@ struct AuxiliaryPreviewPresets {
       transitionConfigEntrance: .syncedToMenuEntranceTransition,
       transitionExitPreset: .fade
     ),
-    dataEntries: [
-      .labelDesc(
-        label: "Entrance Type",
-        desc: "syncedToMenuEntranceTransition"
-      )
-    ]
+    dataEntries: []
   ),];
   
   var presetCounter = 0;
@@ -49,7 +44,161 @@ struct AuxiliaryPreviewPresets {
   };
   
   var currentDataEntries: [DebugDataCardViewController.DataEntry] {
-    self.currentPreset.dataEntries;
+    var baseEntries: [DebugDataCardViewController.DataEntry] = [];
+    
+    let currentConfig = self.currentPreset.config;
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "presetCounter",
+        desc: "\(presetCounter)"
+      )
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "currentPresetIndex",
+        desc: "\(currentPresetIndex)"
+      )
+    );
+    
+    baseEntries.append(
+      .spacing(size: 10)
+    );
+    
+    baseEntries.append(
+      .title(title: "AuxiliaryPreviewConfig")
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "verticalAnchorPosition",
+        desc: currentConfig.verticalAnchorPosition.rawValue
+      )
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "alignmentHorizontal",
+        desc: currentConfig.alignmentHorizontal.rawValue
+      )
+    );
+    
+    if let preferredWidth = currentConfig.preferredWidth {
+      baseEntries.append(
+        .labelDesc(
+          label: "preferredWidth",
+          desc: "\(preferredWidth)"
+        )
+      );
+    };
+    
+    if let preferredHeight = currentConfig.preferredHeight {
+      baseEntries.append(
+        .labelDesc(
+          label: "preferredHeight",
+          desc: "\(preferredHeight)"
+        )
+      );
+    };
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "marginInner",
+        desc: "\(currentConfig.marginInner)"
+      )
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "marginOuter",
+        desc: "\(currentConfig.marginOuter)"
+      )
+    );
+    
+    baseEntries.append(
+      .spacing(size: 10)
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "Entrance Transition Config",
+        desc: ""
+      )
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "Config",
+        desc: currentConfig.transitionConfigEntrance.description
+      )
+    );
+    
+    if let transitionAnimationConfig = currentConfig.transitionConfigEntrance.transitionAnimationConfig {
+      baseEntries.append(
+        .labelDesc(
+          label: "delay",
+          desc: transitionAnimationConfig.delay.description
+        )
+      );
+      
+      baseEntries.append(
+        .labelDesc(
+          label: "animatorConfig",
+          desc: transitionAnimationConfig.animatorConfig.description
+        )
+      );
+      
+      baseEntries.append(
+        .labelDesc(
+          label: "animatorConfig - duration",
+          desc: {
+            let animator = transitionAnimationConfig.animatorConfig.createAnimator(
+              gestureInitialVelocity: .zero
+            );
+            
+            return "\(animator.duration)";
+          }()
+        )
+      );
+      
+      baseEntries.append(
+        .labelDesc(
+          label: "transitionPreset",
+          desc: transitionAnimationConfig.transitionPreset.description
+        )
+      );
+    };
+    
+    baseEntries.append(
+      .spacing(size: 10)
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "Exit Transition Config",
+        desc: ""
+      )
+    );
+    
+    baseEntries.append(
+      .labelDesc(
+        label: "Config",
+        desc: currentConfig.transitionExitPreset.description
+      )
+    );
+    
+    let presetEntries = self.currentPreset.dataEntries;
+    
+    if presetEntries.count > 0 {
+      baseEntries.append(
+        .spacing(size: 10)
+      );
+      
+      baseEntries += presetEntries;
+    };
+    
+    return baseEntries;
   };
   
   mutating func nextPreset(){
