c54d5bc8b46a5fe13c954240bc01a172ab9203bc
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-15 02:18:29 +0800

    ⭐️ Impl: Ex - `DebugDataCardController`
    Related:
    * `TODO:2023-10-22-09-09-34` - Impl: Examples - Add VC for testing the different possible combinations of aux. preview config.

---
+++ Example/Screens/DebugDataCardController.swift
@@ -0,0 +1,117 @@
+//
+//  DebugDataCardController.swift
+//  ContextMenuAuxiliaryPreviewExample
+//
+//  Created by Dominic Go on 11/14/23.
+//
+
+import UIKit
+
+class DebugDataCardViewController: UIViewController {
+
+  enum DataEntry {
+    case labelDesc(label: String, desc: String);
+  };
+  
+  weak var rootStack: UIStackView?;
+  
+  var debugData: [DataEntry] = [
+    .labelDesc(label: "Hello", desc: "World"),
+    .labelDesc(label: "Hello", desc: "World"),
+    .labelDesc(label: "Hello", desc: "World"),
+  ];
+  
+  func updateViews(){
+    guard let rootStack = self.rootStack else { return };
+    
+    rootStack.subviews.forEach {
+      $0.removeFromSuperview();
+    };
+  
+    self.debugData.forEach {
+      switch $0 {
+        case let .labelDesc(labelString, descString):
+          let stack: UIStackView = {
+            let stack = UIStackView();
+            stack.axis = .horizontal;
+            stack.spacing = 5;
+            
+            return stack;
+          }();
+          
+          let label: UILabel = {
+            let label = UILabel();
+            label.text  = "\(labelString):";
+            
+            return label;
+          }();
+          
+          stack.addArrangedSubview(label);
+          
+          let desc: UILabel = {
+            let label = UILabel();
+            label.text  = descString;
+            
+            return label;
+          }();
+          
+          stack.addArrangedSubview(desc);
+          
+          rootStack.addArrangedSubview(stack);
+          
+      };
+    };
+  };
+  
+  override func viewDidLoad() {
+    super.viewDidLoad();
+    
+    self.view.backgroundColor = UIColor(
+      hue: 0,
+      saturation: 0,
+      brightness: 0.95,
+      alpha: 1
+    );
+    
+    self.view.layer.cornerRadius = 10;
+    
+    let rootStack: UIStackView = {
+      let stack = UIStackView();
+      stack.axis = .vertical;
+      
+      return stack;
+    }();
+    
+    self.rootStack = rootStack;
+    self.updateViews();
+    
+    rootStack.translatesAutoresizingMaskIntoConstraints = false;
+    self.view.addSubview(rootStack);
+    
+    let margins: CGFloat = 12;
+    
+    NSLayoutConstraint.activate([
+      rootStack.topAnchor.constraint(
+        equalTo: self.view.layoutMarginsGuide.topAnchor,
+        constant: margins
+      ),
+      rootStack.leadingAnchor.constraint(
+        equalTo: self.view.layoutMarginsGuide.leadingAnchor,
+        constant: margins
+      ),
+      rootStack.trailingAnchor.constraint(
+        equalTo: self.view.layoutMarginsGuide.trailingAnchor,
+        constant: -margins
+      ),
+      rootStack.bottomAnchor.constraint(
+        equalTo: self.view.layoutMarginsGuide.bottomAnchor,
+        constant: -margins
+      ),
+      
+      self.view.heightAnchor.constraint(
+        equalTo: rootStack.heightAnchor,
+        constant: margins * 2
+      ),
+    ]);
+  };
+};
