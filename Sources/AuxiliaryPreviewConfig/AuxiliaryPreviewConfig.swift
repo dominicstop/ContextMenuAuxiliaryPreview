774bd55c1d9b8dc006346aa4f83100cf2233d5c5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-22 17:41:04 +0800

    🛠 Refactor: Replace Usage with `DGSwiftUtilities`
    Summary:
    * Remove `AnimationConfig+CustomStringConvertible`, `HorizontalAlignmentPosition`, `VerticalAnchorPosition`, `VerticalAnchorPositionMode`, `UIView+Helpers`, and replace usage with imports from `DGSwiftUtilities`.

--- Sources/AuxiliaryPreviewConfig/AuxiliaryPreviewConfig.swift
+++ Sources/AuxiliaryPreviewConfig/AuxiliaryPreviewConfig.swift
@@ -6,6 +6,7 @@
 //
 import Foundation
 import UIKit
+import DGSwiftUtilities
 
 
 public struct AuxiliaryPreviewConfig {
--- Sources/Common/AuxiliaryPreviewMetadata.swift
+++ Sources/Common/AuxiliaryPreviewMetadata.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import DGSwiftUtilities
 
 
 public struct AuxiliaryPreviewMetadata {
